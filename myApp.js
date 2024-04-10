d960b9b79c1b16b21b7c00fa874dfc3135961c43
Eren S <esarac@outlook.com.tr>
2024-04-10 00:32:48 +0000

    add createManyPeople handler

--- myApp.js
+++ myApp.js
@@ -27,7 +27,10 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
